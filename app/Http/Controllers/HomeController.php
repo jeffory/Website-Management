69e9f34e57d31c5976e79c188e157f161fd818fb
Keith McGahey <jeffory@c0d.in>
2017-01-21 08:47:55 +1000

    Updated dashboard home
    Gives an overview of tickets open.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Ticket;
+use Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,10 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $ticket_count = Ticket::where('user_id', Auth::user()->id)->count();
+
+        return view('home', [
+            'ticket_count' => $ticket_count
+        ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,29 @@
 
 @section('content')
 <div class="container main-container">
-    <h2>Dashboard</h2>
+    <h2>Welcome back {{ Auth::user()->name }}!</h2>
+    <br>
 
-    <p>Welcome back {{ Auth::user()->name }}!</p>
+    <div class="columns">
+        <div class="column is-3 box has-text-centered icon-box" onclick="Turbolinks.visit('{{ url('tickets') }}')">
+            <a href="{{ url('tickets') }}">
+            <i class="icon fa fa-ticket is-big"></i>
+            @if(Auth::user()->is_admin)
+                <h3>Support Tickets</h3>
+            @else
+                <h3>My Tickets</h3>
+            @endif
+            </a>
+
+            <p>
+                @if(Auth::user()->is_admin)
+                <a href="{{ url('tickets') }}">There are {{ $ticket_count }} ticket(s) open</a>
+                @else
+                <a href="{{ url('tickets') }}">You have {{ $ticket_count }} ticket(s) open</a>
+                @endif
+            </p>
+        </div>
+        
+    </div>
 </div>
 @endsection
