75e02931d6c3c568f54a2f5559a8ea2f64892be9
Keith McGahey <jeffory@c0d.in>
2017-01-22 17:01:11 +1000

    Testing Laravel Echo Auth
    TODO: Fix this code to actually check user/ticket relation.

--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -19,8 +19,12 @@ class BroadcastServiceProvider extends ServiceProvider
         /*
          * Authenticate the user's personal channel...
          */
-        Broadcast::channel('App.User.*', function ($user, $userId) {
-            return (int) $user->id === (int) $userId;
+        Broadcast::channel('ticket.*', function ($user, $ticketId) {
+            return true;
+        });
+
+        Broadcast::channel('ticket.newMessage', function ($user) {
+            return true;
         });
     }
 }
