b70fbba592cde198db2d038822b26dfd02e9b28d
Keith McGahey <jeffory@c0d.in>
2017-02-21 15:48:04 +1000

    Another dumb typo
    Or unedited copy n' pasta rather.

--- config/filesystems.php
+++ config/filesystems.php
@@ -57,7 +57,7 @@ return [
         's3' => [
             'driver' => 's3',
             'key' => getenv('S3_KEY'),
-            'secret' => getenv('S3_KEY'),
+            'secret' => getenv('S3_SECRET'),
             'region' => getenv('S3_REGION'),
             'bucket' => getenv('S3_BUCKET'),
         ],
