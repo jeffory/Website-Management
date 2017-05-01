0bc0717af0eecfb253c22698c24343e263aadac4
Keith McGahey <jeffory@c0d.in>
2017-05-01 10:20:03 +1000

    Added RemoteServer Policy to AuthServiceProvider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -13,8 +13,8 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        'App\Model' => 'App\Policies\ModelPolicy',
-        'App\Ticket' => 'App\Policies\TicketPolicy'
+        'App\Ticket' => 'App\Policies\TicketPolicy',
+        'App\RemoteServer' => 'App\Policies\RemoteServerPolicy'
     ];
 
     /**
