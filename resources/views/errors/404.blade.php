7999d7541c79beb80d2bc2f986243a20e1a58993
Keith McGahey <jeffory@c0d.in>
2017-05-03 21:30:38 +1000

    Added 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>File not found</title>
+        
+        <link href="/css/app.css" rel="stylesheet" type="text/css">
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                display: table;
+                width: 100%;
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .fa {
+                color: #bf5329;
+                font-size: 9em;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="box" style="padding: 3em 1em;">
+                <i class="fa fa-times-circle-o is-huge" aria-hidden="true"></i>
+
+                <h1 class="title is-1">File not found</h1>
+
+                <a href="/" class="button is-dark">Back to home</a>
+            </div>
+        </div>
+    </body>
+</html>
