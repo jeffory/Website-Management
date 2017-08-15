25e691ede2e6a530dbe4e9b27f6fd733bd3a1941
Keith McGahey <jeffory@c0d.in>
2017-08-15 15:04:53 +1000

    Update frontpage text

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -12,7 +12,7 @@
 
             <h2>What we offer</h2>
 
-            <p>With a strong programming background (over 15 years experience) we offer solutions to suit your needs - not just workarounds.</p>
+            <p>With a strong programming background we offer solutions to suit your needs - not just workarounds.</p>
         </div>
 
         <div class="column">
@@ -24,7 +24,7 @@
 
             <h2>Who are we</h2>
 
-            <p>Currently Geckode is the work of a solo developer based in Brisbane, Australia. It was created as a side business as wholesale support to several businesses.</p>
+            <p>Currently Geckode is the work of a solo developer based in Brisbane, Australia. It is a side business for wholesale support to several business partners.</p>
         </div>
 
         <div class="column">
@@ -36,7 +36,7 @@
 
             <h2>Technologies we use</h2>
 
-            <p>Apart from the basic HTML, CSS, JavaScript, and PHP languages, we also specialise in the Laravel framework, and Wordpress platforms. Vue.js also being a strong point.</p>
+            <p>Apart from the basic HTML, CSS, JavaScript, and PHP languages, we specialise in developing applications in the Laravel framework, with Vue.js also being a strong point.</p>
         </div>
     </div>
 </section>
